dff0798fb1f82ef7c50769ef99fbaef2dcc8a1ce
c2p31047 <115206538+c2p31047@users.noreply.github.com>
2024-10-23 00:56:58 +0900

    config.py を更新

--- app/config/config.py
+++ app/config/config.py
@@ -4,7 +4,6 @@ class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'your-secret-key'
-    GOOGLE_MAPS_API_KEY = 'AIzaSyA0kUv4LGrAKsapFLVtlXJe-kySxpsarzA'
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration."""
